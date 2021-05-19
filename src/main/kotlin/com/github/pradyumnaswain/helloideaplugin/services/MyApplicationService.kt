bbf8fd9ff39c4b3710739f1f423dc24e02ed1ccc
GitHub Action <action@github.com>
2021-05-19 15:58:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pradyumnaswain/helloideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pradyumnaswain.helloideaplugin.services
+
+import com.github.pradyumnaswain.helloideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
